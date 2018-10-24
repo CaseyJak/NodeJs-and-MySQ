b5d9dee804ad3750a57a76ff3776b91b978121eb
Casey Jak <casey.j.lee@gmail.com>
2018-10-24 18:37:51 -0500

    started inquirer prompts

--- customerBamazon.js
+++ customerBamazon.js
@@ -5,7 +5,7 @@ let inquirer = require("inquirer");
 
 //Connecttion info to MySQL Database
 let connection = mysql.createConnection({
-    host: "local host" ,
+    host: "localhost" ,
     port: 3306,
     user: "root",
     password: "",
@@ -14,7 +14,7 @@ let connection = mysql.createConnection({
 
 //Connect to MySQL Database
 connection.connect(function(err){
-    if (err){ throw err};
+    if (err) throw err;
     console.log("connected as id " +  connection.threadId);
     console.log("Welcome to Bamazon!");
     shop(); //Function to start Customer Engagment
@@ -24,13 +24,20 @@ connection.connect(function(err){
 // Customer Prompt
 function shop() {
     // Displays all Products from Database
-    connection.query ("SELECT * FROM products", function(err, results){
+    connection.query ("SELECT * FROM products", function(err, res){
         if (err) throw err;
-        //Ask User what they would like to buy
-        inquirer.prompt([
-            {
-
-            }
-        ]);
+        for (let i = 0; i < res.length; i++) {
+            console.log("\ID: " + res[i].id + 
+            "   Product: " + res[i].product_name + 
+            "           Price: " + "$" + res[i].price);
+        }
     });
+    inquirer.prompt([
+        {
+            name: "item",
+            type: "input",
+            message: "Please type the ID # of the item would you like to buy."
+        }
+    ]).then(function (answer))
 }
+
